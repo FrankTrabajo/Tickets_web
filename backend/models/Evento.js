44e58d713dcb12cc007c097fbc52b88e69e7ff63
Francisco Garcia Gomez <152178489+FrankTrabajo@users.noreply.github.com>
2025-04-23 09:30:54 +0200

    Pagina de creacion de eventos para el administrador
    Se ha cambiado el modelo de eventos, ahora guardará grupos de entradas por cada evento, también se ha creado la vista de creación de eventos

--- backend/models/Evento.js
+++ backend/models/Evento.js
@@ -5,7 +5,16 @@ const eventoSchema = new mongoose.Schema({
   descripcion: { type: String },
   fecha: { type: Date, required: true },
   lugar: { type: String, required: true },
-  precio: { type: mongoose.Types.Decimal128, required: true },
+  entradas: { type: [Object] }, // Aqui guardará el tipo de entrada, la cantidad de entradas que hay y el precio
+  /**
+   * por ejemplo
+   * { tipo: grada ,
+   *    cantidad: 100,
+   *    precio: 70€ },
+   * { tipo: pista ,
+   *    cantidad: 160,
+   *    precio: 90€ },
+   */
   capacidad: { type: Number, required: true },
   imagen: { type: String },
   creado_por: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' }
